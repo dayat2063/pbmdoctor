ee38778c0726a5342262c0c3b5212f4ee9acd730
Achmad Nur Hidayat <61482223+dayat2063@users.noreply.github.com>
2023-06-05 20:01:44 +0700

    Initial commit

---
+++ lib/pages/detail_list_pasien.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pbmdoctor/theme.dart';
+
+class detaillistpasien extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              'Detail Antrian',
+              style: blackSemiBoldTextStyle.copyWith(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/detail_list_cards.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class detailistcards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      child: Column(),
+    );
+  }
+}
