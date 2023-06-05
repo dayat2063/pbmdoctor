4d2d8a11972b56acc0a749ce325705c3314a4873
Achmad Nur Hidayat <61482223+dayat2063@users.noreply.github.com>
2023-06-05 21:44:09 +0700

    Update detail_list_pasien.dart

--- lib/pages/detail_list_pasien.dart
+++ lib/pages/detail_list_pasien.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pbmdoctor/theme.dart';
 import 'package:pbmdoctor/widgets/detail_list_cards.dart';
+import 'homepage_doctor.dart';
+import 'absen_doctor.dart';
+import 'package:pbmdoctor/models/navbar.dart';
+import 'package:pbmdoctor/widgets/bottom_navbar_item.dart';
+import 'page_list_pasien.dart';
 
 class detaillistpasien extends StatelessWidget {
   @override
