c3aebdac3c17fba4ed541dda4d8ca7dd2e291c66
Achmad Nur Hidayat <61482223+dayat2063@users.noreply.github.com>
2023-06-05 22:19:53 +0700

    tambah menu di detail list pasien

--- lib/pages/detail_list_pasien.dart
+++ lib/pages/detail_list_pasien.dart
@@ -1,33 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:pbmdoctor/theme.dart';
 import 'package:pbmdoctor/widgets/detail_list_cards.dart';
-// import 'homepage_doctor.dart';
-// import 'absen_doctor.dart';
-// import 'package:pbmdoctor/models/navbar.dart';
-// import 'package:pbmdoctor/widgets/bottom_navbar_item.dart';
-// import 'page_list_pasien.dart';
+import 'homepage_doctor.dart';
+import 'absen_doctor.dart';
+import 'package:pbmdoctor/models/navbar.dart';
+import 'package:pbmdoctor/widgets/bottom_navbar_item.dart';
+import 'page_list_pasien.dart';
 
 class detaillistpasien extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Detail Antrian',
+                style: blackSemiBoldTextStyle.copyWith(fontSize: 20),
+              ),
+              SizedBox(
+                height: 46,
+              ),
+              detailistcards(),
+            ],
+          ),
+        ),
+      ),
+      //AWAL MENU BOTTON
+      floatingActionButton: Container(
+        height: 81,
+        width: 400,
+        decoration: BoxDecoration(
+          color: WhiterColor,
+          border: Border(
+            top: BorderSide(
+              color: greyColor,
+              width: 3,
+            ),
+          ),
+        ),
         child: Column(
           children: [
             SizedBox(
-              height: 50,
+              height: 15.5,
             ),
-            Text(
-              'Detail Antrian',
-              style: blackSemiBoldTextStyle.copyWith(fontSize: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => homepagedoctor(),
+                      ),
+                    );
+                  },
+                  child: Bottomnavbaritem(
+                    Navbar(id: 1, image: 'assets/icons/home_unactive.png'),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => pagelistpasien(),
+                      ),
+                    );
+                  },
+                  child: Bottomnavbaritem(
+                    Navbar(id: 2, image: 'assets/icons/list_active.png'),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Absendoctor(),
+                      ),
+                    );
+                  },
+                  child: Bottomnavbaritem(
+                    Navbar(id: 3, image: 'assets/icons/calender_unactive.png'),
+                  ),
+                ),
+                Bottomnavbaritem(
+                  Navbar(id: 4, image: 'assets/icons/user_unactive.png'),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 46,
-            ),
-            detailistcards(),
+//ini
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
